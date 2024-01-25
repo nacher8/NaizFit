77fa53437aab471cc37e018c78abe5b0bdfe3384
Ignacio Hernaiz <nacho_celtic@hotmail.com>
2024-01-25 09:22:46 +0100

    Initial Commit

---
+++ NaizFit/NaizFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaizFitApp.swift
+//  NaizFit
+//
+//  Created by IGNACIO HERNAIZ IZQUIERDO on 25/1/24.
+//
+
+import SwiftUI
+
+@main
+struct NaizFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
